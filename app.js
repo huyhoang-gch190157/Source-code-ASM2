7e514f066d007b75ece58de6ed2542d6a051d014
huyhoang-gch190157 <77625101+huyhoang-gch190157@users.noreply.github.com>
2021-03-13 10:40:03 +0700

    make it better

--- app.js
+++ app.js
@@ -17,53 +17,46 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }))
 
 //xóa sp
-app.get('/delete',async (req,res)=>{
-    let id = req.query.pid;
-    //var ObjectID = require('mongodb').ObjectID;
-    console.log(id)
-    let condition = {"_id":id};    ``
+app.get('/delete', async (req,res)=>{
+    let id =req.query.pid;
+    let condition = {"_id":id};
 
-    let client= await MongoClient.connect(url);
+    let client = await MongoClient.connect(url);
     let dbo = client.db("productDB2");
-    
-    await dbo.collection("productDetails").deleteOne(condition);
-    res.redirect('/');
+    let results = await dbo.collection("productDetails").deleteOne(condition);
+    res.redirect("/")
 })
 
 //hiển thị trang home
-app.get('/',async (req,res)=>{
-    let client= await MongoClient.connect(url);
+app.get('/', async (req,res)=>{
+    let client = await MongoClient.connect(url);
     let dbo = client.db("productDB2");
-    let results = await dbo.collection("productDetails").find({}).toArray();
-    res.render('home',{model:results})
+    let results = await dbo.collection("productDetails").find({}).toArray({});
+    res.render('home', {model: results})
 })
+
 app.get('/new',(req,res)=>{
     res.render('newProduct')
 })
 
 //add sp
-app.post('/insert',async (req,res)=>{
-    let client= await MongoClient.connect(url);
+app.post('/insert', async( req,res)=>{
+    let client = await MongoClient.connect(url);
     let dbo = client.db("productDB2");
     let IDInput = req.body._id;
     let nameInput = req.body.productName;
-    let priceInput = req.body.price;
+    let priceInput =req.body.price;
     let quantityInput = req.body.quantity;
     let colorInput = req.body.color;
-    console.log(IDInput)
-    console.log(nameInput)
-    console.log(priceInput)
-    console.log(quantityInput)
-    console.log(colorInput)
-    let newProduct = {_id:IDInput, productName : nameInput, price:priceInput, quantity: quantityInput, color: colorInput};   
-    if(IDInput == "" || nameInput  == "" || priceInput  == "" || quantityInput  == "" || colorInput  == "" ){
-        res.redirect('/');
-    } 
-    else{       
+    let newProduct={_id:IDInput, productName:nameInput, price:priceInput, quantity: quantityInput, color: colorInput};
+    if( IDInput == "" ||nameInput == "" ||priceInput == "" ||quantityInput == "" || colorInput == ""){
+        res.redirect('/')
+    }
+    else{
         await dbo.collection("productDetails").insertOne(newProduct);
-        let results = await dbo.collection("productDetails").find({}).toArray();
-        res.render('home',{model:results})
-    } 
+        let results = await dbo.collection("productDetails").find({}).toArray({});
+        res.render('home', {model: results})
+    }
 })
 
 //Search
@@ -72,7 +65,7 @@ app.post('/search',async (req,res)=>{
     let client= await MongoClient.connect(url);
     let dbo = client.db("productDB2");
     let results = await dbo.collection("productDetails").
-        find({productName: new RegExp(searchText,'i')}).toArray();
+    find({productName: new RegExp(searchText,'i')}).toArray();
         
     res.render('home',{model:results})
 })
